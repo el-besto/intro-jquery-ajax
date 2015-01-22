3c027e6ec2665e60db2e7f9644b5f4dceb16b97f
Angelo Cisneros <a.e.cisneros@gmail.com>
2015-01-22 08:53:53 -0800

    Append restaurants to restaurant-container div.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,26 +15,94 @@
 //= require turbolinks
 //= require_tree .
 
-// wait for the page to load 
-$(function (){
-  // make a req to get all articles
-  $.get('/restaurants.json')
-  // wait for it to finish
-   .done(function (data){
-    console.log(data);
-   });
 
-  // post to `restaurant.json`
-  // the `.json` tells our rails app
-  // that we want `json` back
-  // after it creates something
-  $.post('/restaurants.json', {
-          restaurant: {
-            name: "restaurant created from post request to /restaurants.json"
-          }
-  })
-  // newly created restaurant
-  .done (function (data) {
-    console.log("New Restaurant", data)
-  });
+////////////////////
+// exercise part 1: 
+//// Iterate throught all the restaurants and append them to the body
+// $(function () {
+//   var $body = $("body");
+//   $.get("/restaurants.json")
+//     .done(function (restaurants) {
+//       restaurants.forEach(function (restaurant) {
+//         $body.append('<div>' + restaurant.name + '</div>');
+//       });
+//     });
+// });
+////////////////////
+
+
+////////////////////
+// exercise part 2: 
+//// Add an div with id of restaurants-conatiner to restaurants/index.html.erb.
+//// Append all the elements to that div instead.
+$(function () {
+  var $container = $("#restaurants-container");
+  $.get("/restaurants.json")
+    .done(function (restaurants) {
+      restaurants.forEach(function (restaurant) {
+        $container.append("<div>" + restaurant.name + "</div>");
+      });
+    });
+
 });
+
+////////////////////
+
+var restaurantQuery = function (q){
+  return $.ajax({
+    url: '/restaurants.json',
+    data: {s: q},
+    dataType: 'json',
+  }).promise();
+};
+
+// var processData = function (result){
+//   var $div = $('<div/>');
+//   $.each(result, function (i,e){
+//     $div.append($('<p/>')).html(e.Name);
+//   });
+//   $('#restaurants-container').html($.div);
+// }
+
+
+// $(document).ready(function(){
+//   var $restaurantsContainer = $('#restaurants-container');
+//   $.get('/restaurants.json')
+//   .done(function(restaurants){
+//     restaurants.forEach(function(restaurant){
+//       $restaurantContainer.append("<div>" + restaurant.name + "</div>");
+//     });
+//   });
+
+
+
+  // // run initial ajax script
+  // $.get('/restaurants.json')
+  // // wait for it to finish
+  //  .done(function (result){
+  //   console.log(result);
+
+  //   var $div = $('<div/>');
+  //   $.each(result, function (i,e){
+  //     $div.append($('<p/>')).html(e.name);
+  //   });
+  //   $('#restaurants-container').html($.div);
+  //  });
+
+
+  // // on form submission, run the query
+  // $('#queryForm').submit(function(event){
+  //   // first prevent the page from reloading
+  //   event.preventDefault();
+
+  //   // Now setup and run the query
+  //   var query = $('#query').val();
+  //   if (query !== ""){
+  //     console.log("query is valid.");
+  //     restaurantQuery(query).done(processData);
+  //   } else {
+  //     console.log("bad query");
+  //   }
+  // });
+
+// });
